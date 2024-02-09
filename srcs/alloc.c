7e547e021c3e8c84e703064866098186d4f1728d
戸部翔太 <tobeshouta@Intel.local>
2024-02-10 03:19:23 +0900

    [refactor] refacator alloc.c func

--- srcs/alloc.c
+++ srcs/alloc.c
@@ -1,36 +1,37 @@
 #include "tetris.h"
 
-void *xmalloc(size_t size)
+void	*xmalloc(size_t size)
 {
-	void *p = malloc(size);
+	void	*p;
+
+	p = malloc(size);
 	if (p == NULL)
-    {
+	{
 		fprintf(stderr, "Memory allocation error\n");
 		exit(EXIT_FAILURE);
 	}
-	return p;
+	return (p);
 }
 
 t_mino *duplicatet_mino(t_mino mino)
 {
-    t_mino *new_shape = (t_mino *)xmalloc(sizeof(t_mino));
-    char **copyshape = mino.mino_array;
+	t_mino *new_mino = (t_mino *)xmalloc(sizeof(t_mino));
 
-    new_shape->mino_size = mino.mino_size;
-    new_shape->mino_array = (char **)xmalloc(sizeof(char *) * (new_shape->mino_size + 1));
-    for (int i = 0; i < new_shape->mino_size; i++)
+	new_mino->mino_size = mino.mino_size;
+	new_mino->mino_array = (char **)xmalloc(sizeof(char *) * (new_mino->mino_size + 1));
+	for (int i = 0; i < new_mino->mino_size; i++)
 	{
-        new_shape->mino_array[i] = (char *)xmalloc(sizeof(char) * (new_shape->mino_size + 1));
-        memcpy(new_shape->mino_array[i], copyshape[i], new_shape->mino_size);
-    }
-	new_shape->current_row = mino.current_row;
-	new_shape->current_col = mino.current_col;
-    return new_shape;
+		new_mino->mino_array[i] = (char *)xmalloc(sizeof(char) * (new_mino->mino_size + 1));
+		memcpy(new_mino->mino_array[i], mino.mino_array[i], new_mino->mino_size);
+	}
+	new_mino->current_row = mino.current_row;
+	new_mino->current_col = mino.current_col;
+	return new_mino;
 }
 
 void	free_array(char **array)
 {
-	if (array != NULL || array[0] != NULL)
+	if (array != NULL)
 		return ;
 	for (int i = 0; array[i] != NULL; i++)
 	{
