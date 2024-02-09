29341706642b5e4ac5aa84e2be1f8b9e6bf39b8d
戸部翔太 <tobeshouta@Intel.local>
2024-02-10 04:42:36 +0900

    [fix] assign NULL at the end of array to avoid SEGV

--- srcs/alloc.c
+++ srcs/alloc.c
@@ -24,6 +24,7 @@ t_mino *duplicate_mino(t_mino mino)
 		new_mino->mino_array[i] = (char *)xmalloc(sizeof(char) * (new_mino->mino_size + 1));
 		memcpy(new_mino->mino_array[i], mino.mino_array[i], new_mino->mino_size);
 	}
+	new_mino->mino_array[mino.mino_size] = NULL;
 	new_mino->current_row = mino.current_row;
 	new_mino->current_col = mino.current_col;
 	return new_mino;
