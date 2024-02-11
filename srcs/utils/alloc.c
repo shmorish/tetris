5a531ceddb5ff623326ada64c2addb3220f49a27
戸部翔太 <tobeshouta@Intel.local>
2024-02-11 22:43:30 +0900

    [refactor] alloc_utils.c functions that are only used in alloc.c are grouped together as static functions in alloc.c.

--- srcs/utils/alloc.c
+++ srcs/utils/alloc.c
@@ -1,5 +1,27 @@
 # include "tetris.h"
 
+static void	*xmalloc(size_t size)
+{
+	void	*p;
+
+	p = malloc(size);
+	if (p == NULL)
+	{
+		fprintf(stderr, "Memory allocation error\n");
+		exit(EXIT_FAILURE);
+	}
+	return (p);
+}
+
+static void	*memdup(const void *src, size_t size)
+{
+	char	*dest;
+
+	dest = (void *)xmalloc(size + 1);
+	memcpy(dest, src, size);
+	return (dest);
+}
+
 t_player	*init_struct(void)
 {
 	t_player	*player;
